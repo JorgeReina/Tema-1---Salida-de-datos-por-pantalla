7ca9ee487f0bc0957256d290f1258781a576b334
Jorge Reina Romero <116582548+JorgeReina@users.noreply.github.com>
2022-11-13 18:47:11 +0100

    Add files via upload

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -12,8 +12,8 @@ public class Ejercicio2 {
 	public static void main(String[] args){
 		
 		System.out.println("Jorge Reina Romero");
-		System.out.println("Dirección: C/La Gomera 28");
-		System.out.println("Teléfono: 616634004");
+		System.out.println("Dirección: C/Aprobado");
+		System.out.println("Teléfono: 123456789");
 		
 	}
 }
